a4d392cf9c282a431c1633a3f2a8506be80ae77c
Tony <jaal.profissional@gmail.com>
2024-04-17 21:10:14 -0300

    schedule: add Schedule middleware

--- src/schedule/schedule.module.ts
+++ src/schedule/schedule.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ScheduleService } from './schedule.service';
 import { ScheduleController } from './schedule.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ScheduleMiddleware } from './middleware/schedule.middleware';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ScheduleController],
-  providers: [ScheduleService, ScheduleService],
+  providers: [ScheduleService, ScheduleService, ScheduleMiddleware],
 })
 export class ScheduleModule {}
